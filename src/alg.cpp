3844236f062f2a7f5373575c8cf80dbc57edcb4a
Чернавин Данила <78860817+ChernavinD@users.noreply.github.com>
2021-05-09 17:20:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,7 +13,7 @@ return n;
 
 int countPairs2(int* arr, int len, int value) {
 int n = 0, l = 0, r = len - 1;
-for (r; arr[r] > value; r--){}
+for (r; arr[r] > value; r--) {}
 for (r; r > 0; r--) {
 for (l; l < r; l++)
 if (arr[l] + arr[r] == value)
@@ -30,8 +30,8 @@ int l = i + 1, r = len, x = value - arr[i];
 for (; l < r;) {
 int mid = l + (r - l) / 2;
 if (arr[mid] == x) {
-for (; arr[mid - 1] == x && mid > l; mid--){}
-for (; arr[mid] == x; mid++, n++){}
+for (; arr[mid - 1] == x && mid > l; mid--) {}
+for (; arr[mid] == x; mid++, n++) {}
 break;
 } else if (arr[mid] < x) {
 l = mid + 1;
